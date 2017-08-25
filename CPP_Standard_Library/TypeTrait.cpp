ce70b1eab3096682302143d8724cf49c7b1f0639
llssyy404 <christian404@naver.com>
2017-08-25 13:01:17 +0900

    boolalpha 수정

--- CPP_Standard_Library/TypeTrait.cpp
+++ CPP_Standard_Library/TypeTrait.cpp
@@ -15,32 +15,32 @@ using namespace std;
 
 int main()
 {
-	cout << boolalpha << is_void<void>::value << endl;				// true
-	cout << boolalpha << is_integral<short>::value << endl;			// true
-	cout << boolalpha << is_floating_point<double>::value << endl;	// true
-	cout << boolalpha << is_array<int[]>::value << endl;			// true
-	cout << boolalpha << is_pointer<int*>::value << endl;			// true
-	cout << boolalpha << is_reference<int&>::value << endl;			// true
+	cout << boolalpha << is_void<void>::value << endl;		// true
+	cout << is_integral<short>::value << endl;				// true
+	cout << is_floating_point<double>::value << endl;		// true
+	cout << is_array<int[]>::value << endl;					// true
+	cout << is_pointer<int*>::value << endl;				// true
+	cout << is_reference<int&>::value << endl;				// true
 
 	struct A {
 		int a;
 		int f(int) { return 2011; }
 	};
-	cout << boolalpha << is_member_object_pointer<int A::*>::value << endl;			// true
-	cout << boolalpha << is_member_function_pointer<int (A::*)(int)>::value << endl;// true
+	cout << is_member_object_pointer<int A::*>::value << endl;			// true
+	cout << is_member_function_pointer<int (A::*)(int)>::value << endl;	// true
 
 	enum E {
 		e = 1,
 	};
-	cout << boolalpha << is_enum<E>::value << endl;					// true
+	cout << is_enum<E>::value << endl;						// true
 
 	union U {
 		int u;
 	};
-	cout << boolalpha << is_union<U>::value << endl;				// true
+	cout << is_union<U>::value << endl;						// true
 
-	cout << boolalpha << is_class<string>::value << endl;			// true
-	cout << boolalpha << is_function<int * (double)>::value << endl;// true
-	cout << boolalpha << is_lvalue_reference<int&>::value << endl;	// true
-	cout << boolalpha << is_rvalue_reference<int&&>::value << endl;	// true
+	cout << is_class<string>::value << endl;				// true
+	cout << is_function<int * (double)>::value << endl;		// true
+	cout << is_lvalue_reference<int&>::value << endl;		// true
+	cout << is_rvalue_reference<int&&>::value << endl;		// true
 }
--- CPP_Standard_Library/TypeTrait2.cpp
+++ CPP_Standard_Library/TypeTrait2.cpp
@@ -15,18 +15,16 @@ using namespace std;
 
 int main()
 {
-	cout << boolalpha << is_const<int>::value << endl;						// false
-	cout << boolalpha << is_const<const int>::value << endl;				// true
-	cout << boolalpha << is_const<add_const<int>::type>::value << endl;		// true
+	cout << boolalpha << is_const<int>::value << endl;			// false
+	cout << is_const<const int>::value << endl;					// true
+	cout << is_const<add_const<int>::type>::value << endl;		// true
 
 	typedef add_const<int>::type myConstInt;
-	cout << boolalpha << is_const<myConstInt>::value << endl;				// true
+	cout << is_const<myConstInt>::value << endl;				// true
 
 	typedef const int myConstInt2;
-	cout << boolalpha << is_same<myConstInt, myConstInt2>::value << endl;	// true
+	cout << is_same<myConstInt, myConstInt2>::value << endl;	// true
 
-	cout << boolalpha << 
-		is_same<int, remove_const<add_const<int>::type>::type>::value << endl;		// true
-	cout << boolalpha << 
-		is_same<const int, add_const<add_const<int>::type>::type>::value << endl;	// true
+	cout << is_same<int, remove_const<add_const<int>::type>::type>::value << endl;		// true
+	cout << is_same<const int, add_const<add_const<int>::type>::type>::value << endl;	// true
 }
